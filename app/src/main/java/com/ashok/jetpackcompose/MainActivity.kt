bd4b8bf6614c0cfc05c56b7a3ee68270d6d2d534
ashokthorat <ashokthorat@gmail.com>
2024-05-10 12:08:51 +0530

    aZXzZZ

---
+++ app/src/main/java/com/ashok/jetpackcompose/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.ashok.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ashok.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Ashok",
+                    )
+                }
+            }
+        }
+    }
+    fun show() {
+        var bc = 10
+        bc = 50
+        bc = 56
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello $name!",
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeTheme {
+        Greeting("Ashok")
+    }
+}
